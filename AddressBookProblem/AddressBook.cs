f5998418c14f9ca31995c9e914f6c3fdec3e5220
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-10-01 15:32:04 +0530

    [Sai]Added. AddressBook class

---
+++ AddressBookProblem/AddressBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookProblem
+{
+    class AddressBook
+    {
+        public AddressBook(String firstName, String lastName, String address, String city, String state, string zip, string phoneNumber, String email)
+        {
+            Contact add = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+        }
+    }
+}
